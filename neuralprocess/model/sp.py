83d712615a313d7a39b4015922ae3e103f39cc56
Jens Petersen <mail@jens.pe>
2020-04-09 20:37:24 +0200

    Saving...

--- neuralprocess/model/sp.py
+++ neuralprocess/model/sp.py
@@ -193,3 +193,63 @@ class SegmentationProcess(nn.Module):
 
         # decode
         return self.decode(context_representation)
+
+
+
+class AttentiveSegmentationProcess(SegmentationProcess):
+
+    def __init__(self, attention, global_sum=True, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.attention = attention
+        self.global_sum = global_sum
+
+    def aggregate(self, context_query, target_query, context_representation, target_representation=None):
+        """
+        Aggregate representations. This implementation uses attention over queries/keys.
+
+        Args:
+            context_query (torch.tensor): Shape (B, N, Cq).
+                These are the keys for the attention mechanism.
+            target_query (torch.tensor): Shape (B, M, Cq).
+                These are the queries for the attention mechanism.
+            context_representation (torch.tensor): Shape (B, N, Ci, ...).
+                These are the values for the attention mechanism.
+                Can also be a list or tuple of tensors with varying
+                number of channels and spatial size.
+            target_representation (torch.tensor): Shape (B, M, Ci, ...).
+                Will just be concatenated to the output in this implementation.
+
+        Returns:
+            torch.tensor: Average representation.
+                Will be a list if input is a list or tuple.
+
+        """
+
+        if torch.is_tensor(context_representation):
+            context_representation = [context_representation, ]
+        if torch.is_tensor(target_representation):
+            target_representation = [target_representation, ]
+        if target_representation is None:
+            target_representation = [None, ] * len(context_representation)
+
+        for r, rep in enumerate(context_representation[:-1]):
+            concat = match_shapes(rep, target_representation[r], target_query, ignore_axes=2)
+            if target_representation[r] is None:
+                concat = concat[:1]
+            else:
+                concat = concat[:2]
+            context_representation[r] = torch.cat(concat, 2)
+
+        # either use summation or attention in the lowest layer
+        if self.global_sum:
+            context_representation[-1] = context_representation[-1].mean(1, keepdim=True)
+            context_representation[-1] = torch.cat(
+                match_shapes(context_representation[-1], target_representation[-1], target_query, ignore_axes=2),
+                2
+            )
+        else:
+            pass
+
+        return context_representation
