bc72e3647d83632f2df531a8cefe39bce1fa9959
Jens Petersen <mail@jens.pe>
2020-04-15 22:31:19 +0200

    Pull up SegmentationProcess in init

--- neuralprocess/model/__init__.py
+++ neuralprocess/model/__init__.py
@@ -1,3 +1,4 @@
 from .np import NeuralProcess
 from .anp import AttentiveNeuralProcess
 from .convcnp import ConvDeepSet, GPConvDeepSet, ConvCNP
+from .sp import SegmentationProcess, AttentiveSegmentationProcess
