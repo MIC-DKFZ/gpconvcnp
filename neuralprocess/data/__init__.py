4f29412f9a928362c5d3626703df4a5d5b1489d1
Jens Petersen <jens.petersen@dkfz.de>
2021-06-11 15:24:01 +0200

    fix broken imports in inits

--- neuralprocess/data/__init__.py
+++ neuralprocess/data/__init__.py
@@ -3,4 +3,3 @@ from .step import StepFunctionGenerator
 from .population import LotkaVolterraGenerator, get_lynx_hare_data
 from .fourier import FourierSeriesGenerator
 from .weather import TemperatureGenerator
-from .health import ECGGenerator
--- neuralprocess/model/__init__.py
+++ neuralprocess/model/__init__.py
@@ -1,4 +1,3 @@
 from .np import NeuralProcess
 from .anp import AttentiveNeuralProcess
 from .convcnp import ConvDeepSet, GPConvDeepSet, ConvCNP
-from .sp import SegmentationProcess, AttentiveSegmentationProcess
